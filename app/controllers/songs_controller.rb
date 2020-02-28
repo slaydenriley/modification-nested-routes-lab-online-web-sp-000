b38051b3bbb8944e4df6ea9f0195f4cea5806f59
Riley Slayden <slaydenriley@gmail.com>
2020-02-28 19:20:38 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,10 +43,16 @@ class SongsController < ApplicationController
   end
 
   def edit
-    if params[:artist_id] && !Artist.exists?(params[:artist_id])
-      redirect_to artists_path, alert: "Artist not found"
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @song = artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(artist), alert: "Song not found" if @song.nil?
+      end
     else
-      @song = Song.update(artist_id: params[:artist_id])
+      @song = Song.find(params[:id])
     end
   end
 
